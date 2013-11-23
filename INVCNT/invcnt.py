25dfef9f15e9d6855c058ec294820e6cb957f080
Manav Kataria <mk.newsgrp@gmail.com>
2013-11-23 02:11:22 +0000

    Intermediate Checkin. Infinite Loop

--- INVCNT/invcnt.py
+++ INVCNT/invcnt.py
@@ -11,12 +11,67 @@ import logging
 
 DEBUG = True
 
-def process(N, data):
-    for i in range(0,N):
-        if (data[i] != 42):
-            print(data[i])
+def countInvMerge(array, start, middle, end):
+    logging.debug('Merge array:')
+    logging.debug(array)
+    logging.debug('Merge: start %d, middle %d, end %d', start, middle, end)
+
+    tmpArray = []
+
+    i = start
+    j = middle+1
+
+    invcount=0
+
+    while(i<=middle and j <= end):
+        logging.debug('Merge: (%d <= %d)?', array[i], array[j])
+
+        if (array[i] <= array[j]):
+            tmpArray.append(array[i])
+            i+=1
         else:
-            return
+            tmpArray.append(array[j])
+            j+=1
+            invcount+=(middle-i+1)
+
+    #copy left array left-over into tmpArray
+    while(i<=middle):
+        tmpArray.append(array[i])
+        i+=1
+
+    #Copy tmpArray back to array
+    for i in range(start,start+len(tmpArray)):
+        array[i] = tmpArray[i-start]
+
+    return invcount
+
+
+def countInvSplit(array, start, end):
+    logging.debug('Split array:')
+    logging.debug(array)
+    logging.debug('Split: start %d, end %d', start, end)
+    if ((end-start+1) <= 1): return 0
+    else:
+        cl = countInvSplit(array, start, end/2)
+        cr = countInvSplit(array, end/2+1, end)
+        cm = countInvMerge(array, start, end/2, end)
+
+        logging.debug('cr(%d), cl(%d), cm(%d), sum(%d)', cr, cl, cm, cr+cl+cm)
+
+        return (cl+cr+cm)
+
+def process(N, data):
+    global DEBUG
+    DEBUG = True
+
+    for i in range(0,N):
+        logging.info('Processing Array:')
+        logging.info(data[i])
+
+        result = countInvSplit(data[i][1],0,data[i][0])
+
+        print('InvCount: %d' % result)
+
 
 def read_array(n):
     arr = []
