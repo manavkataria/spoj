e00a5f83b910d9629cd31625255adf44bc39483f
Manav Kataria <mk.newsgrp@gmail.com>
2013-12-06 02:52:31 +0000

    Add colors

--- myUnit.sh
+++ myUnit.sh
@@ -3,17 +3,25 @@
 # cd ./INVCNT/ (directory containg source-code)
 # ../myUnit.sh invcnt.py
 
+rst=$(tput sgr0)
+txtbld=$(tput bold)             # Bold
+bldred=${txtbld}$(tput setaf 1) #  red
+bldgre=${txtbld}$(tput setaf 2) #  green
+bldblu=${txtbld}$(tput setaf 4) #  blue
+warn=${bldred}-${txtrst}
+pass=${bldblu}-${txtrst}
+
 echo "== Running Tests =="
 
 for infile in $(ls in.*)
 do
-        echo -en "$infile \t- "
+        echo -en "$infile \t"
         ./$1 < $infile &> result.tmp
         outfile=`echo $infile | sed 's/in\./out\./g'`
         diff $outfile result.tmp > diff.tmp
         if [ -s 'diff.tmp' ]; 
-            then echo "Fail!"; 
-            else echo "Success!"; 
+            then echo "$warn Fail! $rst"; 
+            else echo "$pass Success! $rst"; 
         fi
         rm diff.tmp result.tmp
 done
